ce47dd1c13977fbebd6d1e76902c4034e39507c3
VoidByte12 <166216903+VoidByte12@users.noreply.github.com>
2025-09-26 19:39:55 +0100

    extra_txts

--- Code&Texts/Calculations.py
+++ Code&Texts/Calculations.py
@@ -5,6 +5,8 @@ for i in range(1,3):
         analysis_lvl = 'word'
     if i == 2:
         analysis_lvl = 'phoneme'
-    print(Functions.analyse("C_Julius_Caesar_De_bello_Gallico_COMMENTARIUS_PRIMUS_(bk1).txt", analysis_level = analysis_lvl))
-    print(Functions.analyse("C_Julius_Caesar_Gallic_War_bk1.txt", analysis_level = analysis_lvl))
+    # print(Functions.analyse("C_Julius_Caesar_De_bello_Gallico_COMMENTARIUS_PRIMUS_(bk1).txt", analysis_level = analysis_lvl))
+    # print(Functions.analyse("C_Julius_Caesar_Gallic_War_bk1.txt", analysis_level = analysis_lvl))
+test = {Functions.analyse("C_Julius_Caesar_De_bello_Gallico_COMMENTARIUS_PRIMUS_(bk1).txt", analysis_level = analysis_lvl)}
+print(test)
     
